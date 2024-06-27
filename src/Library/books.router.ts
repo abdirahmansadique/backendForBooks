074593baf1ead77c5b2fd959a0673dbe94c4b522
abdirahmansadique <abdirahmansadique01@gmail.com>
2024-06-27 14:23:33 +0300

    routing

--- src/Library/books.router.ts
+++ src/Library/books.router.ts
@@ -2,8 +2,8 @@ import {Hono} from 'hono'
 import {getAllBooks,getBook , createBook, updateBook,deleteBookC} from './books.controller'
 
 export const LibraryRouter=new Hono();
-LibraryRouter.get('/AllBooks', getAllBooks);
-LibraryRouter.get('/OneBook/:id', getBook);
+LibraryRouter.get('/books', getAllBooks);
+LibraryRouter.get('/books/:id', getBook);
 LibraryRouter.post('/books', createBook);
-LibraryRouter.put('/BBooks/:id',  updateBook);
-LibraryRouter.delete('/del/:id', deleteBookC);
+LibraryRouter.put('/books/:id',  updateBook);
+LibraryRouter.delete('/books/:id', deleteBookC);
